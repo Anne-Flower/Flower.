e66307f926b29b505113b8566b237dfaed9670f6
Anne Flore Bernard <b.anneflore@yahoo.fr>
2025-05-15 23:13:53 +0200

    start with css

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,9 +1,9 @@
-export default function Header() {
-  return (
-    <header className="w-full px-6 py-4 border-b border-secondary">
-      <div className="max-w-7xl mx-auto flex items-center justify-between">
-        <h1 className="text-title font-heading">Anne-Flore Bernard</h1>
-      </div>
-    </header>
-  )
-}
+// export default function Header() {
+//   return (
+//     <header className="w-full px-6 py-4 border-b border-secondary">
+//       <div className="max-w-7xl mx-auto flex items-center justify-between">
+//         <h1 className="text-title font-heading">Anne-Flore Bernard</h1>
+//       </div>
+//     </header>
+//   )
+// }
